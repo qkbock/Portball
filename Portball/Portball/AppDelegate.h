dbbae7018b8fe692196d65f95fafd38b5a26d215
Quincy Bock <qkbock@gmail.com>
2014-03-11 17:07:12 -0400

    first commit

---
+++ Portball/Portball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Portball
+//
+//  Created by Quincy Bock on 3/11/14.
+//  Copyright (c) 2014 Aero + Quincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Portball/Portball/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Portball
+//
+
+//  Copyright (c) 2014 Aero + Quincy. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
