46a1cd667e1b662dbf2bf0fc8c8ec13c78619d03
Quincy Bock <qkbock@gmail.com>
2014-03-11 22:52:04 -0400

    with bouncing ball and background image

--- Portball/Portball/MyScene.m
+++ Portball/Portball/MyScene.m
@@ -9,9 +9,22 @@
 
 #import "MyScene.h"
 
+
+typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
+{
+    CNPhysicsCategoryBall = 1 << 0, // 0001 = 1
+    CNPhysicsCategoryFloor = 1 << 1, // 0010 = 2
+    CNPhysicsCategoryEnemy = 1 << 2, // 0100 = 4
+    CNPhysicsCategoryFriend = 1 << 3, // 1000 = 8
+};
+
+@interface MyScene()<SKPhysicsContactDelegate>
+@end
+
 @implementation MyScene
 {
-    SKSpriteNode *_circle;
+    SKSpriteNode *_ball;
+//    SKShapeNode *_ball;
 }
 
 //--------------------------------------------------------------------------
@@ -30,6 +43,13 @@
 - (void)initializeScene
 {
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    SKSpriteNode* bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+    bg.position = CGPointMake(self.size.width/2, self.size.height/2);
+    
+    self.physicsWorld.contactDelegate = self;
+    self.physicsBody.categoryBitMask = CNPhysicsCategoryFloor;
+    
+    [self addChild: bg];
     [self spawnBall];
 
 }
@@ -38,9 +58,26 @@
 
 -(void)spawnBall
 {
-    _circle = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
-    _circle.position = CGPointMake(self.size.width * 0.50, self.size.height * 0.50);
-    [self addChild:_circle];
+    //with an image
+    _ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    _ball.position = CGPointMake(self.size.width/8, self.size.height/2);
+    
+    _ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:_ball.size.width/2];
+    _ball.name = @"ball";
+    _ball.physicsBody.restitution = 1.0;
+//    _ball.physicsBody.density = 20.0;
+    _ball.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+
+
+    [self addChild:_ball];
+    
+    //With a path
+//    _ball = [SKShapeNode node];
+//    CGRect box = CGRectMake(self.size.width/4, self.size.height/2, 20, 20);
+//    UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:box];
+//    _ball.path = circlePath.CGPath;
+//    [self addChild:_ball];
+
 }
 
 //--------------------------------------------------------------------------
