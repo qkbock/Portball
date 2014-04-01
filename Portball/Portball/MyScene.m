efcbc63f7467fe8bce4f104ef61567dc4f301be8
Quincy Bock <qkbock@gmail.com>
2014-04-01 18:39:35 -0400

    bunny assembled

--- Portball/Portball/MyScene.m
+++ Portball/Portball/MyScene.m
@@ -24,6 +24,7 @@ typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
     CNPhysicsCategoryShelf  = 1 << 4,
     CNPhysicsCategoryWhite  = 1 << 5,
     CNPhysicsCategoryBlack  = 1 << 6,
+    CNPhysicsCategoryInert  = 1 << 7,
 };
 
 @interface MyScene()<SKPhysicsContactDelegate>
@@ -39,6 +40,7 @@ typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
     SKNode *_bgLayer;
     SKNode *_portalNode;
     SKNode *_ballNode;
+    SKNode *_bunnyNode;
     CGPoint _touchLocation;
     NSTimeInterval _lastUpdateTime;
     NSTimeInterval _dt;
@@ -83,6 +85,11 @@ typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
     _ballNode = [SKNode node];
     [self addChild:_ballNode];
     [self spawnBall:CGPointMake(20, 160)];
+    
+    _bunnyNode = [SKNode node];
+    [self addChild:_bunnyNode];
+    [self spawnBunny:CGPointMake(self.size.width/2, self.size.height/2)];
+    
 }
 
 -(void)moveBG
@@ -274,6 +281,159 @@ typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
 
 //--------------------------------------------------------------------------
 
+-(void)spawnBunny:(CGPoint)position
+{
+    SKSpriteNode *_backLeg = [SKSpriteNode spriteNodeWithImageNamed:@"backLeg"];
+    SKSpriteNode *_backArm = [SKSpriteNode spriteNodeWithImageNamed:@"backArm"];
+    SKSpriteNode *_backEar = [SKSpriteNode spriteNodeWithImageNamed:@"backEar"];
+    SKSpriteNode *_tail = [SKSpriteNode spriteNodeWithImageNamed:@"tail"];
+    SKSpriteNode *_body = [SKSpriteNode spriteNodeWithImageNamed:@"body"];
+    SKSpriteNode *_frontArm = [SKSpriteNode spriteNodeWithImageNamed:@"frontArm"];
+    SKSpriteNode *_frontEar = [SKSpriteNode spriteNodeWithImageNamed:@"frontEar"];
+    SKSpriteNode *_frontLeg = [SKSpriteNode spriteNodeWithImageNamed:@"frontLeg"];
+    SKSpriteNode *_jaw = [SKSpriteNode spriteNodeWithImageNamed:@"jaw"];
+    
+//    SKSpriteNode *_earSpringHook = [SKSpriteNode spriteNodeWithImageNamed:@"hook"];
+    SKNode *_earSpringHook = [SKNode node];
+    SKNode *_jawSpringHook = [SKNode node];
+    SKNode *_armSpringHook = [SKNode node];
+    SKNode *_legSpringHook = [SKNode node];
+
+    
+    _backLeg.position = CGPointMake(position.x - 1.811, position.y - 21.889); //X
+    _backArm.position = CGPointMake(position.x + 8.623, position.y - 9.081 ); //X
+    _backEar.position = CGPointMake(position.x - 0.564, position.y + 43.339); //X
+    _tail.position = CGPointMake(position.x - 24.746, position.y - 31.555); //X
+    _body.position = position; //X
+    _frontArm.position = CGPointMake(position.x + 3.654, position.y - 10.44); //X
+    _frontEar.position = CGPointMake(position.x - 6.64, position.y + 40.222); //X
+    _frontLeg.position = CGPointMake(position.x - 6.048, position.y - 26.655); //X
+    _jaw.position = CGPointMake(position.x + 15.822, position.y + 16.962);
+    
+    _earSpringHook.position = CGPointMake(position.x -8.536, position.y + 69.237);
+    _jawSpringHook.position = CGPointMake(position.x +20.813, position.y + 31.891);
+    _armSpringHook.position = CGPointMake(position.x +27.388, position.y - 12.729);
+    _legSpringHook.position = CGPointMake(position.x +15.535, position.y + -14.443);
+
+    
+    _backLeg.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_backLeg.size];
+    _backArm.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_backArm.size];
+    _backEar.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_backEar.size];
+    _tail.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_tail.size];
+    _body.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_body.size];
+    _frontArm.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_frontArm.size];
+    _frontEar.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_frontEar.size];
+    _frontLeg.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_frontLeg.size];
+    _jaw.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_jaw.size];
+    
+    _earSpringHook.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:3];
+    _jawSpringHook.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:3];
+    _armSpringHook.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:3];
+    _legSpringHook.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:3];
+
+    
+    _backLeg.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _backArm.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _backEar.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _tail.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _body.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _frontArm.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _frontEar.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _frontLeg.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    _jaw.physicsBody.categoryBitMask = CNPhysicsCategoryBall;
+    
+    _backLeg.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _backArm.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _backEar.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _tail.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _frontArm.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _frontEar.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _frontLeg.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    _jaw.physicsBody.collisionBitMask = CNPhysicsCategoryFloor;
+    
+    _earSpringHook.physicsBody.categoryBitMask = CNPhysicsCategoryInert;
+    _earSpringHook.physicsBody.collisionBitMask = kNilOptions;
+    _jawSpringHook.physicsBody.categoryBitMask = CNPhysicsCategoryInert;
+    _jawSpringHook.physicsBody.collisionBitMask = kNilOptions;
+    _armSpringHook.physicsBody.categoryBitMask = CNPhysicsCategoryInert;
+    _armSpringHook.physicsBody.collisionBitMask = kNilOptions;
+    _legSpringHook.physicsBody.categoryBitMask = CNPhysicsCategoryInert;
+    _legSpringHook.physicsBody.collisionBitMask = kNilOptions;
+    
+    [_bunnyNode addChild: _backLeg];
+    [_bunnyNode addChild: _backArm];
+    [_bunnyNode addChild: _backEar];
+    [_bunnyNode addChild: _tail];
+    [_bunnyNode addChild: _body];
+    [_bunnyNode addChild: _frontArm];
+    [_bunnyNode addChild: _frontEar];
+    [_bunnyNode addChild: _frontLeg];
+    [_bunnyNode addChild: _jaw];
+    
+    [_bunnyNode addChild: _earSpringHook];
+    [_bunnyNode addChild: _jawSpringHook];
+    [_bunnyNode addChild: _armSpringHook];
+    [_bunnyNode addChild: _legSpringHook];
+
+    
+    SKPhysicsJointPin *leg1Joint = [SKPhysicsJointPin jointWithBodyA:_backLeg.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x - 7.95, position.y - 18.74)];
+    SKPhysicsJointPin *leg2Joint = [SKPhysicsJointPin jointWithBodyA:_frontLeg.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x - 11.909, position.y - 24.312)];
+    SKPhysicsJointPin *arm1Joint = [SKPhysicsJointPin jointWithBodyA:_backArm.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x + 6.713, position.y - 1.731 )];
+    SKPhysicsJointPin *arm2Joint = [SKPhysicsJointPin jointWithBodyA:_frontArm.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x + 0.88, position.y - 2.904)];
+    SKPhysicsJointPin *ear1Joint = [SKPhysicsJointPin jointWithBodyA:_backEar.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x + 2, position.y + 33.779)];
+    SKPhysicsJointPin *ear2Joint = [SKPhysicsJointPin jointWithBodyA:_frontEar.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x - 0.472, position.y + 31.553)];
+    SKPhysicsJointPin *tailJoint = [SKPhysicsJointPin jointWithBodyA:_tail.physicsBody
+                                                               bodyB:_body.physicsBody
+                                                              anchor:CGPointMake(position.x - 23.639, position.y - 31.644)];
+    SKPhysicsJointSliding *jawJoint = [SKPhysicsJointSliding jointWithBodyA:_jaw.physicsBody bodyB:_body.physicsBody anchor:CGPointMake(position.x + 12.578, position.y + 18.503) axis: CGVectorMake(0, .1)];
+    
+    SKPhysicsJointFixed *earSpringJointFix = [SKPhysicsJointFixed jointWithBodyA:_earSpringHook.physicsBody bodyB:_body.physicsBody anchor:_body.position ];
+    SKPhysicsJointFixed *jawSpringJointFix = [SKPhysicsJointFixed jointWithBodyA:_jawSpringHook.physicsBody bodyB:_body.physicsBody anchor:_body.position ];
+    SKPhysicsJointFixed *armSpringJointFix = [SKPhysicsJointFixed jointWithBodyA:_armSpringHook.physicsBody bodyB:_body.physicsBody anchor:_body.position ];
+    SKPhysicsJointFixed *legSpringJointFix = [SKPhysicsJointFixed jointWithBodyA:_legSpringHook.physicsBody bodyB:_body.physicsBody anchor:_body.position ];
+    
+    SKPhysicsJointSpring *ear1SpringJoint = [SKPhysicsJointSpring jointWithBodyA:_earSpringHook.physicsBody bodyB:_backEar.physicsBody anchorA:_earSpringHook.position anchorB:CGPointMake(position.x - 2.818, position.y + 52.228)];
+    SKPhysicsJointSpring *ear2SpringJoint = [SKPhysicsJointSpring jointWithBodyA:_earSpringHook.physicsBody bodyB:_frontEar.physicsBody anchorA:_earSpringHook.position anchorB:CGPointMake(position.x - 12.642, position.y + 49.148)];
+
+    
+    jawJoint.shouldEnableLimits = YES;
+    jawJoint.lowerDistanceLimit = 3;
+    jawJoint.upperDistanceLimit = 4;
+    ear1SpringJoint.damping = -10;
+    ear2SpringJoint.damping = -10;
+
+    [self.physicsWorld addJoint:leg1Joint];
+    [self.physicsWorld addJoint:leg2Joint];
+    [self.physicsWorld addJoint:arm1Joint];
+    [self.physicsWorld addJoint:arm2Joint];
+    [self.physicsWorld addJoint:ear1Joint];
+    [self.physicsWorld addJoint:ear2Joint];
+    [self.physicsWorld addJoint:tailJoint];
+    [self.physicsWorld addJoint:jawJoint];
+
+    [self.physicsWorld addJoint:earSpringJointFix];
+    [self.physicsWorld addJoint:jawSpringJointFix];
+    [self.physicsWorld addJoint:armSpringJointFix];
+    [self.physicsWorld addJoint:legSpringJointFix];
+    
+    [self.physicsWorld addJoint:ear1SpringJoint];
+    [self.physicsWorld addJoint:ear2SpringJoint];
+
+}
+
+//--------------------------------------------------------------------------
 -(void)update:(CFTimeInterval)currentTime
 {
     if (_lastUpdateTime) {
